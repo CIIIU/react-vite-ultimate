b18bd2a7b82f9c45fc31940d1f99ff218d9f7349
Duong Quoc Hung <quochung11052003@gmail.com>
2024-09-24 14:41:06 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,14 @@
+
+import './style.css';
+
 const MyComponent = () => {
     return(
+        <>
       <div>Hung @ react</div>
+      <div className="halo"
+            style={{borderRadius: "10px"}}
+      > HALO</div>
+      </>
     );
   }
   export default MyComponent
