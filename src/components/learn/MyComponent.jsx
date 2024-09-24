9aed2ad5da6f8f5764217fb6cc0c67eb3b597f65
Duong Quoc Hung <quochung11052003@gmail.com>
2024-09-24 14:53:21 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,9 +2,20 @@
 import './style.css';
 
 const MyComponent = () => {
+    // const name ="Hung dep trai"; //string
+    // const name =26; //number
+
+    // const name =[1,2,3]; //array
+    const name ={
+        name: "hung dep trai",
+        age: 22
+    }
+
     return(
         <>
-      <div>Hung @ react</div>
+      <div>{JSON.stringify(name)} @ react</div>
+
+      <div>{console.log("hung")}</div>
       <div className="halo"
             style={{borderRadius: "10px"}}
       > HALO</div>
