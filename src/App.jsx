1ee3be54268d3875797dfa763fa3c6ef3e571586
Duong Quoc Hung <quochung11052003@gmail.com>
2024-09-25 09:39:34 +0700

    video 40

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,11 @@ import reactLogo from './assets/react.svg'
 import { useState } from 'react';
 
 const App = () => {
-  const name ="hung";
-  const age = 22;
-  const data = {
-    address: "hanoi",
-    country: "Viet Nam"
-  }
+  
 
   const [todoList, setTodoList] = useState([
-    { id: 1, name: "Learning React " },
-    { id: 2, name: "Watching Youtube" }
+    // { id: 1, name: "Learning React " },
+    // { id: 2, name: "Watching Youtube" }
   ])
 
   const addNewTodo = (name) => {
@@ -37,9 +32,7 @@ const App = () => {
       addNewTodo= {addNewTodo}
       />
       <TodoData
-      name = {name}
-      age = {age}
-      data = {data}
+      
       todoList= {todoList}
       />
       <div className='todo-image'>
